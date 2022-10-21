0dcd44c6d6ffa676ee2ddb11ec10b39cacdcfa89
Gintarė Šimkūnienė <gintareba@gmail.com>
2022-10-21 17:15:39 +0300

    feat: add shopping-cart preview

--- e-commerce_page/src/app/app.module.ts
+++ e-commerce_page/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './header/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
