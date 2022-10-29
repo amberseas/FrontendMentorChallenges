80a8751d76ff55819d2f48c2a68b9970dba20cab
Gintarė Šimkūnienė <gintareba@gmail.com>
2022-10-29 17:28:04 +0300

    feat: add header

--- space-tourism-page/src/app/app.module.ts
+++ space-tourism-page/src/app/app.module.ts
@@ -5,12 +5,14 @@ import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
